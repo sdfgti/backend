96251feaa2ebefd93433dd7122d46a918670ad60
Samoluk Volodumur Igorovish <61546278+sdfgti@users.noreply.github.com>
2024-06-20 16:43:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-
-// MongoDB connection
-const dbURI = 'mongodb://localhost:27017/mydatabase'; // Замените mydatabase на название вашей базы данных
+const dbURI = 'mongodb://localhost:27017/mydatabase';
 mongoose.connect(dbURI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
-
-// Define a schema and model for your data
 const cardSchema = new mongoose.Schema({
   title: String,
   type: String,
